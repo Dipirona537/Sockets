7000cb9944536a3876ff6230bed1325b97af0fbb
Dipirona537 <dipirona28@gmail.com>
2023-02-15 12:27:37 -0400

    asdasda

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import Cripto.Criptmessage;
+
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Server ou cliente");
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        /*System.out.println("Server ou cliente");
         Scanner sc = new Scanner(System.in);
         String esc = sc.nextLine();
         if (esc.equals("1"))
@@ -11,5 +14,8 @@ public class Main {
         else{
             Client.Cliente.cliente("localhost", 1500);
         }
+        //a*/
+        System.out.print(Criptmessage.criptografar("testando", "5849"));
     }
+    //https://stackoverflow.com/questions/1132567/encrypt-password-in-configuration-files
 }
